ba5ef0774863b8e735794dcb7541cf2db6387602
kkyrothelil <85583851+kkyrothelil@users.noreply.github.com>
2021-06-08 22:41:22 +0300

    Init commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+	$('.icon-menu').click(function(event){
+		$('.icon-menu,.menu__body').toggleClass('_active');
+		$('body').toggleClass('_lock');
+	});
+});
+var mainSlider = new Swiper('.main-slider__container',{
+	loop: true,
+
+	pagination:{
+		el: '.main-slider__pagination',
+		clickable: true,
+	}
+});
+
+var aboutSlider = new Swiper('.slider-about__container',{
+	loop: true,
+
+	slidesPerView: 'auto',
+});
